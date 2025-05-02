2fbe356a6076c4148f072626f31abf69a11a0a90
Deepika Penmetsa <deepikapenmetsa99@gmail.com>
2025-05-02 03:01:06 -0400

    deployment update frontend 1

--- vite.config.js
+++ vite.config.js
@@ -7,4 +7,12 @@ export default defineConfig({
   server: {
     port: 3000,
   },
+  proxy: {
+    '/api': {
+      target: 'https://jobmatrix.up.railway.app',
+      changeOrigin: true,
+      secure: true,
+      rewrite: (path) => path.replace(/^\/api/, '')
+    }
+  }
 })
